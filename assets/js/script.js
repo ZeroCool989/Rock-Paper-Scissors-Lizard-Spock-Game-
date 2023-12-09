77bff2c8683cfdbe6ece4a64d4cdd1a254e65f3c
ZeroCool989 <>
2023-12-09 10:59:15 +0000

    Implement playerChoice function to handle user selection and game flow

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,21 @@ let playerScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 let isMuted = false; // Track if the game sounds are muted
+
+// Function called when the player makes a choice
+function playerChoice(playerSelection) {
+    // Generate a random choice for the computer
+    const computerSelection = computerChoice();
+    // Display both player's and computer's choices
+    displayChoices(playerSelection, computerSelection);
+    // Determine the winner and get the result message
+    const result = determineWinner(playerSelection, computerSelection);
+    // Update the scoreboard with the new scores
+    updateScoreboard();
+    // Display the result message on the screen
+    document.getElementById('result').innerText = result;
+    // Play the appropriate sound based on the result
+    playSound(result);
+    // Start the bouncing animation for the chosen images
+    startBouncing();
+}
